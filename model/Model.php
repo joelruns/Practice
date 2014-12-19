9cb9662802bfcdcb88160e2972e1973cab5adc6e
joelruns <joel.run@gmail.com>
2014-12-19 02:48:25 -0500

    Folders added

--- model/Model.php
+++ model/Model.php
@@ -13,7 +13,7 @@ class Model {
         if ($session === true) {
             new Session(43200, false, 64800);
         } if ($database === true) {
-            $this->database = new Database('localhost', '8889', 'captions', 'root', 'Sandbox0!');
+            $this->database = new Database('localhost', '8889', 'captions', 'root', 'root');
         }
     }
 
