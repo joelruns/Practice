cd8dbcdb15a3ea213da32fbdc748a3130146a990
Josh Baiad <jbaiad@outlook.com>
2014-12-21 20:34:47 -0500

    DatabaseMapper.php
    DatabaseMapper.php includes the operations for storing the blog data in
    the SQL database

--- model/mapper/DatabaseMapper.php
+++ model/mapper/DatabaseMapper.php
@@ -1,16 +1,39 @@
 <?php
 
-namespace controller\access;
+namespace model\mapper;
 
-use \controller\Controller as Controller;
+use \model\Model as Model;
 
-class TestAccess extends Controller {
+class DatabaseMapper extends Mapper {
 
     public function __construct($model) {
         parent::__construct($model);
         $this->model->pushAddress($_SERVER['REQUEST_URI']);
     }
 
+    public function select_tuple_length() {
+    	$query = "SELECT tuple FROM blog;";
+    	$result = mysql_query($query);
+    	return mysql_fetch_assoc($result)["tuple"];   
+    }
+
+    public function select_blog() {
+    	$query = "SELECT * FROM blog;";
+    	$result = mysql_query($query);
+    	return mysql_fetch_assoc($result);
+    }
+
+    public function insert_tuple($tuple) {
+    	$query = "INSERT INTO blog (tuple)
+    		 	  VALUES $tuple;";
+    	$result = mysql_query($query);
+    }
+
+    public function insert_entry_title($title, $entry_id) {
+    	$query = "INSERT INTO blog_title (title, entry_id)
+    			  VALUES ($title, $entry_id);";
+    	$result = mysql_query($query);
+    }
 }
 
 ?>
